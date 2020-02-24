0f896e91aa5fc0f817ab236691c160716a2862a0
rf-spuds <rfspuds@gmail.com>
2020-02-23 21:43:29 -0500

    added alert if the users criteria was <1

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var lChar = "abcdefghijklmnopqrstuvwxyz";
 var uChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numChar = "1234567890";
-var spChar = "!@#$%^&*=-_'";
+var spChar = "!@#$%^&*=-_";
 
 // Input variables
 var generateBtn = document.querySelector("#generate");
@@ -13,23 +13,82 @@ var userPS = document.getElementById("pwSpecial");
 
 // Output variable
 var passwordText = document.querySelector("#password");
+var passwordResult = "";
 
 // Functions where the magic happens 
+
+
+function criteria(l, characters) {
+    var passwordResult;
+    for (var i = 0; i < l; i++) {
+        passwordResult += characters.charAt(Math.floor(Math.random() * characters.length))
+    };
+    // check if there are numbers
+    if (userPN.value > 0) {
+        passwordResult += getRandomFromArray(numChar);
+    };
+    if (userPN.value > 0) {
+        pwd += getRandomFromArray(numChar);
+    };
+    if (userPS.value > 0) {
+        pwd += getRandomFromArray(spChar);
+    };
+    if (userPU.value > 0) {
+        pwd += getRandomFromArray(lChar.toUpperCase());
+    };
+    return passwordResult;
+}
+
 
 
 function password(l, characters) {
     var pwd = "";
     for (var i = 0; i < l; i++) {
         pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
+    };
+    if (userPU.value < 1) {
+        alert("Your password is not strong enough.")
+    };
+    if (userPS.value < 1) {
+        alert("Your password is not strong enough.")
+    };
+    if (userPN.value < 1) {
+        alert("Your password is not strong enough.")
+    };
+
 
     return pwd;
 };
 
-// event listeners
+// function buildPassword() {
+//     // check for number and proper length of pass
+//     if (userPL >= 8 && userPL <= 128) {
+//         for (var i = 0; i < userPL; i++) {
+//             // TO-DO: check for user password preference
+//             addCharFromArray(lowerCaseArray);
+//         }
+//     } else {
+//         alert("Your password is either too big or too small!")
+//     }
+//     return passwordResult;
+// };
+
+// utility function for getting characters from array
+
+function getRandomFromArray(arr) {
+    return arr[parseInt(Math.random() * arr.length)];
+};
+
+
+
+// event listener main button
 
 generateBtn.addEventListener("click", function() {
-    var characters = lChar
+    var characters = lChar;
+
+    // if (userPU.value > 0) {
+    //     characters += uChar.charAt(Math.floor(Math.random() * uChar.length) * userPU.value);
+    // };
 
     passwordText.value = password(userPL.value, characters);
-})
+});
